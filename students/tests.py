16635236460fa82cabc41312ebcc3b43efa1f4cb
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-03 13:49:19 +0200

    Added unit tests

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,19 @@
+import random
 from django.test import TestCase
+from .models import Student
 
-# Create your tests here.
+
+class StudentModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            student_number=random.randint(10000, 99999),
+            first_name='Bob',
+            last_name='Smith',
+            email='bob.smith@test.com',
+            field_of_study='Computer Science',
+            gpa=3.92
+        )
+
+    def test_student_model(self):
+        data = self.student
+        self.assertIsInstance(data, Student)
